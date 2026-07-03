a776254b7f2590ed3401be7746a3d502965cda12
Tanvi Bidve <tanvibidve6@gmail.com>
2026-07-03 12:35:00 +0530

    toaster changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,10 @@ import { Provider } from 'react-redux'
 import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
     <Provider store={store}>
       <App />
     </Provider>
-    <Toaster />
-  </StrictMode>,
+   
+  
 )
